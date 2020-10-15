5a9c927c58f8f84c2a9ef36616ddfd88ebd84865
Viral Patel <vir@qable.io>
2020-10-15 17:36:35 +0530

    Updated a Doc config file for remove blog option

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ module.exports = {
           label: 'Docs',
           position: 'left',
         },
-        {to: 'blog', label: 'Blog', position: 'left'},
+
         // Please keep GitHub link to the right for consistency.
         {
           href: 'https://github.com/Tesbo/Tesbo-Web',
